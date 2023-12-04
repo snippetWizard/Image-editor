f10760d5e312fca047fed4404be5fc5546f6f872
snippetWizard <paulsagar5433@gmail.com>
2023-12-04 22:58:23 +0530

    added filter and clear button for removing strokes

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+from email.mime import image
 import tkinter as tk
 from tkinter import filedialog, colorchooser
+from tkinter import ttk
+from turtle import width
 from PIL import Image, ImageOps, ImageTk, ImageFilter
 
 pen_color = "black"
@@ -31,6 +34,29 @@ def change_color():
 def change_size(size):
     global pen_size
     pen_size = size
+
+def clear_canvas():
+    canvas.delete("all")
+    canvas.create_image(0,0, image=canvas.image, anchor="nw")
+
+def apply_filter(filter):
+    image = Image.open(file_path)
+    width, height = int(image.width / 2), int(image.height / 2)
+    image = image.resize((width, height), Image.LANCZOS)
+    if filter == "Black and White":
+        image = ImageOps.grayscale(image)
+    elif filter == "Blur":
+        image = image.filter(ImageFilter.BLUR)
+    elif filter == "Sharpen":
+        image = image.filter(ImageFilter.SHARPEN)
+    elif filter == "Smooth":
+        image = image.filter(ImageFilter.SMOOTH)
+    elif filter == "Emboss":
+        image = image.filter(ImageFilter.EMBOSS)
+    image = ImageTk.PhotoImage(image)
+    canvas.image = image
+    canvas.create_image(0,0, image=image, anchor="nw")
+    
 
 
 root = tk.Tk()
@@ -66,20 +92,31 @@ color_button.pack(pady=5)
 
 pen_size_frame = tk.Frame(left_frame, bg="white")
 pen_size_frame.pack(pady=5)
-pen_size_1 = tk.Radiobutton(
-    pen_size_frame, text="Small", value=5, bg="white", command= lambda: change_size(5)
-)
+
+pen_size_1 = tk.Radiobutton(pen_size_frame, text="Small", value=3, bg="white", command=lambda: change_size(5))
 pen_size_1.pack(side="left")
 
-pen_size_2 = tk.Radiobutton(
-    pen_size_frame, text="Medium", value=10, bg="white", command= lambda: change_size(10)
-)
+pen_size_2 = tk.Radiobutton(pen_size_frame, text="Medium", value=5, bg="white", command=lambda: change_size(10))
 pen_size_2.pack(side="left")
+pen_size_2.select()
 
-pen_size_3 = tk.Radiobutton(
-    pen_size_frame, text="Large", value=15, bg="white", command= lambda: change_size(15)
-)
+pen_size_3 = tk.Radiobutton(pen_size_frame, text="Large", value=7, bg="white", command=lambda: change_size(15))
 pen_size_3.pack(side="left")
+
+clear_button = tk.Button(left_frame, text="Clear", command=clear_canvas, bg="#FF9797")
+clear_button.pack(pady=10)
+
+filter_label = tk.Label(left_frame, text="Select Filter", bg="white")
+filter_label.pack()
+
+filter_combobox = ttk.Combobox(left_frame, values=["Black and White", "Blur", "Emboss", "Sharpen", "Smooth"])
+filter_combobox.pack()
+filter_combobox.bind("<<ComboboxSelected>>", lambda event: apply_filter(filter_combobox.get()))
+
+
+
+
+
 
 canvas.bind("<B1-Motion>", draw)
 
